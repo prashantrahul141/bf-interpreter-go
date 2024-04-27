97f67ae013b0b8377a00326d90a13438551c57e9
Prashant <prashantrahul141@protonmail.com>
2024-04-28 03:57:30 +0530

    rename string.go utils to utils.go

--- utils/utils.go
+++ utils/utils.go
@@ -10,3 +10,10 @@ func ReverseString(s string) string {
 	}
 	return string(runes)
 }
+
+// Reverses an array of any type.
+func ReverseArray[T any](s []T) {
+	for i, j := 0, len(s)-1; i < j; i, j = i+1, j-1 {
+		s[i], s[j] = s[j], s[i]
+	}
+}
