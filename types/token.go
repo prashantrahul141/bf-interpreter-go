6ac6d4de6c550c33fa4e8f548c7578ef7c03e714
Prashant <prashantrahul141@protonmail.com>
2024-04-28 03:57:14 +0530

    impl string for token type

--- types/token.go
+++ types/token.go
@@ -1,21 +1,52 @@
 package types
 
+import "fmt"
+
 type TokenType int
 
 const (
-	TokenRightAngle  = iota // >
-	TokenLeftAngle          // <
-	TokenPlus               // +
-	TokenMinus              // -
-	TokenRightSquare        // ]
-	TokenLeftSquare         // [
-	TokenComma              // ,
-	TokenDot                // .
-	TokenEof                // end of file.
+	TokenRightAngle  TokenType = iota // >
+	TokenLeftAngle                    // <
+	TokenPlus                         // +
+	TokenMinus                        // -
+	TokenRightSquare                  // ]
+	TokenLeftSquare                   // [
+	TokenComma                        // ,
+	TokenDot                          // .
+	TokenEof                          // end of file.
 )
+
+// implement Fmt.Stringer for TokenType
+func (ttype TokenType) String() string {
+	switch ttype {
+	case TokenRightAngle:
+		return "TokenType(>)"
+	case TokenLeftAngle:
+		return "TokenType(<)"
+	case TokenPlus:
+		return "TokenType(+)"
+	case TokenMinus:
+		return "TokenType(-)"
+	case TokenRightSquare:
+		return "TokenType(])"
+	case TokenLeftSquare:
+		return "TokenType([)"
+	case TokenComma:
+		return "TokenType(,)"
+	case TokenDot:
+		return "TokenType(.)"
+	case TokenEof:
+		return "TokenType(EOF)"
+	}
+	return "TokenType(unrecognised)"
+}
 
 // Type of a specific token.
 type Token struct {
 	Line       uint32    // in the source file
 	Token_type TokenType // its type.
+}
+
+func (t Token) String() string {
+	return fmt.Sprintf("Token(%s, %d)", t.Token_type, t.Line)
 }
