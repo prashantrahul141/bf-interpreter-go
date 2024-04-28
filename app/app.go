d51a4ad2b91b16f1f1a3f372c9271277002a5bc9
Prashant <prashantrahul141@protonmail.com>
2024-04-29 02:13:43 +0530

    intergrate parser in app

--- app/app.go
+++ app/app.go
@@ -2,6 +2,7 @@ package app
 
 import (
 	"bfigo/lexer"
+	"bfigo/parser"
 	"bfigo/types"
 	"bfigo/utils"
 )
@@ -11,10 +12,15 @@ type App struct {
 
 func (app *App) Run() {
 	sourceContent := utils.GetFileContent()
-	utils.GetGlobalLogger().Debug("File read", "content", sourceContent)
+	utils.GetGlobalLogger().Info("File read", "content", sourceContent)
 
 	var tokens []types.Token
-	lexer := lexer.Lexer{tokens, sourceContent}
-	lexer.ParseTokens()
+	m_lexer := lexer.Lexer{tokens, sourceContent}
+	m_lexer.ParseTokens()
+
+	var opCodes []int32
+	parser := parser.Parser{opCodes, &m_lexer}
+	parser.Parse()
+	utils.GetGlobalLogger().Info("done parsing", "opcode", parser.OpCodes)
 
 }
