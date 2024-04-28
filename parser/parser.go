d8da8391687bf42f0e41e7688d5f89c37886e946
Prashant <prashantrahul141@protonmail.com>
2024-04-29 02:11:38 +0530

    add emiting methods

--- parser/parser.go
+++ parser/parser.go
@@ -15,6 +15,10 @@ type IParser interface {
 	matchToken(types.Token) bool
 	// Returns if there are no more tokens to parse.
 	isEmpty() bool
+	// emits int32
+	emitInt32(int32)
+	// emits op codes
+	emitOpCode(types.OpCode)
 }
 
 // Top level parser
@@ -44,3 +48,13 @@ func (parser *Parser) isEmpty() bool {
 	return len(parser.Lexer.Tokens) <= 0
 }
 
+// emits int32
+func (parser *Parser) emitInt32(t int32) {
+	utils.GetGlobalLogger().Debug("emiting", "type", t)
+	parser.OpCodes = append(parser.OpCodes, t)
+}
+
+// emits op codes
+func (parser *Parser) emitOpCode(opCode types.OpCode) {
+	parser.emitInt32(int32(opCode))
+}
