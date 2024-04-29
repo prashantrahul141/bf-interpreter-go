f2c680322a5a24ed3d5ef244bbac1f20cd56faf4
Prashant <prashantrahul141@protonmail.com>
2024-04-29 18:08:39 +0530

    add vm struct

--- vm/vm.go
+++ vm/vm.go
@@ -1,11 +1,21 @@
 package vm
 
 import "bfigo/utils"
+import (
+	"bfigo/types"
+	"bfigo/utils"
+	"fmt"
+	"os"
+	"time"
+
+	"golang.org/x/term"
+)
 
 // Vm struct.
 type Vm struct {
-	Ip      uint32                    // instruction pointer
-	Dp      uint32                    // data pointer
-	State   [utils.VM_DATA_SIZE]uint8 // Vm's internal state of the memory.
-	OpCodes []int32                   // instructions to execute.
+	Ip      int32                      // instruction pointer
+	Dp      int32                      // data pointer
+	State   [utils.VM_STATE_SIZE]uint8 // Vm's internal state of the memory.
+	OpCodes []int32                    // instructions to execute.
 }
+
